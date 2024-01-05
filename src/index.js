f076d7b748b5fa01ffa78a9e7ba97788b84f1067
SarinaSevda <sarina.schoki@gmail.com>
2024-01-05 19:27:34 +0100

    Added the time data

--- src/index.js
+++ src/index.js
@@ -4,13 +4,34 @@ function updateWeatherInformation(response) {
   let conditionElement = document.querySelector("#condition");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
   temperatureValue.innerHTML = currentTemperature;
+  timeElement.innerHTML = formatDate(date);
   conditionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = response.data.wind.speed;
+}
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
 
-  //console.log(response.data.wind.speed);
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day}, ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
